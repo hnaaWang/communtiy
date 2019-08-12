afdc62eea9e5927f8dce73c6b3eae306f8ff930f
tyggz <jintiannizou@163.com>
2019-08-12 11:41:02 +0800

    session

--- src/main/java/com/mj/communtiy/controller/AuthorizeController.java
+++ src/main/java/com/mj/communtiy/controller/AuthorizeController.java
@@ -4,9 +4,12 @@ import com.mj.communtiy.dto.AccessTokeDTO;
 import com.mj.communtiy.dto.GithubUser;
 import com.mj.communtiy.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by Administrator on 2019/8/9.
@@ -17,17 +20,29 @@ public class AuthorizeController {
     @Autowired
     GitHubProvider gitHubProvider;
 
+
+    @Value("${gitHub.client.clientId}")
+    private String clientId;
+    @Value("${gitHub.client.clientSecret}")
+    private String clientSecret;
+    @Value("${gitHub.redirectUri}")
+    private String redirectUri;
+
+
     @GetMapping("/callback")
     public String callback(@RequestParam(value = "code") String code ,
-                           @RequestParam(value = "state") String state){
+                           @RequestParam(value = "state") String state,HttpServletRequest request){
         AccessTokeDTO accessTokeDTO = new AccessTokeDTO();
-        accessTokeDTO.setClient_id("Iv1.72282759716c3176");
-        accessTokeDTO.setClient_secret("0d59e5403cd8258a7ff999c9139e6845a1f92066");
+        accessTokeDTO.setClient_id(clientId);
+        accessTokeDTO.setClient_secret(clientSecret);
         accessTokeDTO.setCode(code);
-        accessTokeDTO.setRedirect_uri("http://localhost:8999/callback");
+        accessTokeDTO.setRedirect_uri(redirectUri);
         accessTokeDTO.setState(state);
         String accessToken = gitHubProvider.getAccessToken(accessTokeDTO);
         GithubUser user = gitHubProvider.getUser(accessToken);
+        if(user!=null ){
+            request.getSession().setAttribute("user",user);
+        }
         System.out.println(user.toString());
         return "index";
     }
